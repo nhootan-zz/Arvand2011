05ca920d349768df9701f4238c6a7dac701328ac
nhootan <nhootan@gmail.com>
2015-09-18 00:49:51 -0400

    Initial Import

---
+++ src/artificial_domain/artificial_successor_generator.h
@@ -0,0 +1,22 @@
+#ifndef ARTIFICIAL_SUCCESSOR_GENERATOR_H
+#define ARTIFICIAL_SUCCESSOR_GENERATOR_H
+
+#include "../successor_generator.h"
+
+class Operator;
+class State;
+class RHSimulator;
+class MTRand_int32;
+
+
+class ArtificialSuccessorGenerator : public SuccessorGenerator {
+	std::vector<Operator *> operators;
+	RHSimulator* params;
+public:
+	ArtificialSuccessorGenerator(RHSimulator* params);
+	virtual ~ArtificialSuccessorGenerator();
+    virtual void generate_applicable_ops(const State &curr, std::vector<const Operator *> &ops);
+    virtual void _dump(std::string indent);
+};
+
+#endif
---
+++ src/artificial_domain/r_h_simulator.h
@@ -0,0 +1,40 @@
+#ifndef ARTIFICAL_PARAMS_H
+#define ARTIFICAL_PARAMS_H
+
+#include "../simulator.h"
+// based on the Simulator introduced
+// in "Preferred Operators and Deferred Evaluation in Satisficing Planning"
+// by Silvia Richter and Malte Helmert
+
+class Options;
+
+// default values for RHSimulator
+static const int DEFAULT_D = 50;
+static const int DEFAULT_BF = 25;
+static const int DEFAULT_P = 1;
+static const int DEFAULT_R = 1;
+
+class RHSimulator : public Simulator{
+public:	
+	int solution_depth;
+	int branching_factor;
+	int progress;
+	int regress;
+	Heuristic * h_;
+
+	// extern int node_limit;
+	// extern int g_preferred_operator_boost;
+
+	/*int prob_forwards_preferred;
+	int prob_stay_preferred;
+	int prob_backwards_preferred;*/
+	RHSimulator(const Options &opts);
+	virtual void run();
+	virtual void set_heuristic(Heuristic* h) {h_ = h;}
+	virtual Heuristic* get_heuristic() {return h_;}
+	void dump();
+
+};
+
+#endif
+
